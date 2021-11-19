6b0d5c3844a857793dc6dd8c7b378e05206ec7e2
Izzxt <haziqizzat89@gmail.com>
2021-11-19 19:34:35 +0800

    update generate random string

--- Core/Utils.php
+++ Core/Utils.php
@@ -17,6 +17,6 @@ class Utils
   // Copy paste from https://stackoverflow.com/a/13212994/14433899
   public static function generateRandomString($length = 10)
   {
-    return substr(str_shuffle(str_repeat($x = '0123456789zABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
+    return date('yhis') . substr(str_shuffle(str_repeat($x = '0123456789zABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
   }
 }
