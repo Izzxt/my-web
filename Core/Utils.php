4e34e9cfa36a75a5faa6c87a1517be6d47551cc3
Izzxt <haziqizzat89@gmail.com>
2021-11-19 18:39:30 +0800

    add generate random string for order number

--- Core/Utils.php
+++ Core/Utils.php
@@ -13,4 +13,10 @@ class Utils
   {
     return Session::set(['error' => $message]);
   }
+
+  // Copy paste from https://stackoverflow.com/a/13212994/14433899
+  public static function generateRandomString($length = 10)
+  {
+    return substr(str_shuffle(str_repeat($x = '0123456789zABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
+  }
 }
