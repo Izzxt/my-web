293f7abb0148d932c055f76d3b3f7e4b946d4c0d
H4zlq <haziqnaim72@gmail.com>
2021-11-20 12:28:29 +0800

    Add `utils` for profile

--- Core/Utils.php
+++ Core/Utils.php
@@ -2,6 +2,8 @@
 
 namespace Core;
 
+use App\Models\User;
+
 class Utils
 {
   public static function Logger($consoleLogger)
@@ -14,9 +16,36 @@ class Utils
     return Session::set(['error' => $message]);
   }
 
-  // Copy paste from https://stackoverflow.com/a/13212994/14433899
+  /**
+   * Reference Link
+   * @link {https://stackoverflow.com/a/13212994/14433899}
+   */
   public static function generateRandomString($length = 10)
   {
     return date('yhis') . substr(str_shuffle(str_repeat($x = '0123456789zABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
+  }
+
+  /**
+   * Reference Link
+   * @link {https://www.cluemediator.com/partially-hide-email-address-in-php}
+   */
+  public static function hideEmail($email)
+  {
+    // use FILTER_VALIDATE_EMAIL filter to validate an email address
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      // split an email by "@"
+      list($first, $last) = explode("@", $email);
+
+      // get half the length of the first part
+      $len = floor(strlen($first) / 2);
+
+      // partially hide a string by "*" and return full string
+      return substr($first, 0, $len) . str_repeat('*', $len) . "@" . $last;
+    }
+  }
+
+  public static function hidePhoneNumber($phone)
+  {
+    return substr($phone, 0, 2) . '******' . substr($phone, -2);
   }
 }
