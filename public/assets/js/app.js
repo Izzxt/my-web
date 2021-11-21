0edf18baa3097a6657be35f34305f9a90c79d1e8
H4zlq <haziqnaim72@gmail.com>
2021-11-21 08:55:50 +0800

    Convert to ES6 syntax

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,44 +1,54 @@
-this.web_body = null;
-this.web_document = null;
-this.web_window = null;
-this.modal = null;
+class WebInterface {
+  constructor() {
+    this.document = $(document);
+    this.window = $(window);
 
-this.init = function () {
-  this.web_body = $("body");
-  this.web_window = $(window);
-  this.web_document = $(document);
+    this.window.on("scroll", () => {
+      var sticky = $(".navbar");
+      scroll = this.window.scrollTop();
 
-  this.web_window.on("scroll", function () {
-    var sticky = $(".navbar");
-    scroll = this.web_window.scrollTop();
-
-    if (scroll > 0) {
-      sticky.addClass("bg-white shadow-md");
-    } else {
-      sticky.removeClass("bg-white shadow-md");
-    }
-  });
-
-  $(document).ready(() => {
-    this.toggleModal("login");
-    this.toggleModal("register");
-    this.toggleModal("email");
-    this.toggleModal("phone");
-
-    var id = $("input").filter("#item-id");
-    id.each((v, e) => {
-      var id = e.value;
-
-      var value = parseInt($(`.input-number-${id}`).val());
-
-      if (value === 1) {
-        $(`.number-decrement-${id}`).attr("disabled", true);
+      if (scroll > 0) {
+        sticky.addClass("bg-white shadow-md");
       } else {
-        $(`.number-decrement-${id}`).removeAttr("disabled", false);
+        sticky.removeClass("bg-white shadow-md");
       }
     });
-  });
-};
+
+    this.document.ready(() => {
+      this.toggleModal("login");
+      this.toggleModal("register");
+      this.toggleModal("email");
+      this.toggleModal("phone");
+
+      var id = $("input").filter("#item-id");
+      id.each((v, e) => {
+        var id = e.value;
+
+        var value = parseInt($(`.input-number-${id}`).val());
+
+        if (value === 1) {
+          $(`.number-decrement-${id}`).attr("disabled", true);
+        } else {
+          $(`.number-decrement-${id}`).removeAttr("disabled", false);
+        }
+      });
+    });
+  };
+
+  toggleModal(name) {
+    $("." + name + "-open").click(() => {
+      $("." + name + "-modal").toggleClass("opacity-0 pointer-events-none transition-all ease-in-out duration-500");
+    });
+
+    $("." + name + "-close").click(() => {
+      $("." + name + "-modal").toggleClass("opacity-0 pointer-events-none transition-all ease-in-out duration-500");
+    });
+
+    $("." + name + "-overlay").click(() => {
+      $("." + name + "-modal").toggleClass("opacity-0 pointer-events-none transition-all ease-in-out duration-500");
+    });
+  }
+}
 
 function inc(id) {
   var value = parseInt($(`.input-number-${id}`).val());
@@ -54,18 +64,6 @@ function dec(id) {
   $(`.input-number-${id}`).val((value -= 1));
 }
 
-function toggleModal(name) {
-  $("." + name + "-open").click(() => {
-    $("." + name + "-modal").toggleClass("opacity-0 pointer-events-none transition-all ease-in-out duration-500");
-  });
-
-  $("." + name + "-close").click(() => {
-    $("." + name + "-modal").toggleClass("opacity-0 pointer-events-none transition-all ease-in-out duration-500");
-  });
-
-  $("." + name + "-overlay").click(() => {
-    $("." + name + "-modal").toggleClass("opacity-0 pointer-events-none transition-all ease-in-out duration-500");
-  });
-}
-
-this.init();
+$(function () {
+  new WebInterface();
+});
